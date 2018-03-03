6076e51ae182c92239563fdd861141a1e940905b
Keliang Lu <keliang-lu@uiowa.edu>
2018-03-02 18:32:10 -0600

    added file

---
+++ hw4.0/src/java/model/Cars.java
@@ -0,0 +1,75 @@
+
+package model;
+
+public class Cars {
+    private int CARID;
+    private String CARNAME;
+    private int CARYEAR;
+    private String CARMODEL;
+    private String CARCOLOR;
+
+    public Cars() {
+        this.CARID = 0;
+        this.CARNAME = "";
+        this.CARYEAR = 0;
+        this.CARMODEL = "";
+        this.CARCOLOR = "";
+    }
+    
+    
+    public Cars(int CARID, String CARNAME, int CARYEAR, String CARMODEL, String CARCOLOR) {
+        this.CARID = CARID;
+        this.CARNAME = CARNAME;
+        this.CARYEAR = CARYEAR;
+        this.CARMODEL = CARMODEL;
+        this.CARCOLOR = CARCOLOR;
+    }
+    
+    public int getCARID() {
+        return CARID;
+    }
+
+    public void setCARID(int CARID) {
+        this.CARID = CARID;
+    }
+
+    public String getCARNAME() {
+        return CARNAME;
+    }
+
+    public void setCARNAME(String CARNAME) {
+        this.CARNAME = CARNAME;
+    }
+
+    public int getCARYEAR() {
+        return CARYEAR;
+    }
+
+    public void setCARYEAR(int CARYEAR) {
+        this.CARYEAR = CARYEAR;
+    }
+
+    public String getCARMODEL() {
+        return CARMODEL;
+    }
+
+    public void setCARMODEL(String CARMODEL) {
+        this.CARMODEL = CARMODEL;
+    }
+
+    public String getCARCOLOR() {
+        return CARCOLOR;
+    }
+
+    public void setCARCOLOR(String CARCOLOR) {
+        this.CARCOLOR = CARCOLOR;
+    }
+
+    @Override
+    public String toString() {
+        return "Cars{" + "CARID=" + CARID + ", CARNAME=" + CARNAME + ", CARYEAR=" + CARYEAR + ", CARMODEL=" + CARMODEL + ", CARCOLOR=" + CARCOLOR + '}';
+    }
+        
+
+
+}
--- hw4.0/web/index.jsp
+++ hw4.0/web/index.jsp
@@ -1,17 +1,13 @@
-<%-- 
-    Document   : index
-    Created on : Mar 2, 2018, 5:27:41 PM
-    Author     : kellu
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Cars information</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Cars Database</h1>
+        
+        <a href="read">View All Cars</a>
     </body>
 </html>
